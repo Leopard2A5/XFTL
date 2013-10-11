791f3e5a78bd3e190f79223527d60ebb8a41b9d3
Peter <peter.friedland@gmx.de>
2013-10-11 10:02:54 +0200

    corrected code indention

--- xftl-game/src/DesktopGame.java
+++ xftl-game/src/DesktopGame.java
@@ -3,20 +3,20 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class DesktopGame {
 	
-		private static LwjglApplicationConfiguration createConfiguration(){
-			LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        	config.fullscreen = false;
-        	config.height = 768;
-        	config.resizable = false;
-        	config.title = "xftl";
-        	config.useGL20 = false;
-        	config.vSyncEnabled = true;
-        	config.width = 1024;
-        	
-        	return config;
-		}
+	private static LwjglApplicationConfiguration createConfiguration(){
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.fullscreen = false;
+		config.height = 768;
+		config.resizable = false;
+		config.title = "xftl";
+		config.useGL20 = false;
+		config.vSyncEnabled = true;
+		config.width = 1024;
+		
+		return config;
+	}
 	
-        public static void main (String[] args) {
-        	new LwjglApplication(new Game(), createConfiguration());
-        }
+	public static void main (String[] args) {
+		new LwjglApplication(new Game(), createConfiguration());
+	}
 }

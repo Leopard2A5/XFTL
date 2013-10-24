adca99fde8c499fe66c2868f4e0337b06762ea20
René Perschon <perschon@itso-berlin.de>
2013-10-24 16:09:38 +0200

    refactored FireTest

--- xftl-model/src/test/java/de/xftl/model/ships/FireTest.java
+++ xftl-model/src/test/java/de/xftl/model/ships/FireTest.java
@@ -21,15 +21,24 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import de.xftl.spec.model.ships.Deck;
+import de.xftl.spec.model.ships.DeckNumber;
+import de.xftl.spec.model.ships.Room;
+import de.xftl.spec.model.ships.Ship;
 import de.xftl.spec.model.ships.Tile;
+import de.xftl.spec.model.systems.LifeSupport;
 
 public class FireTest {
 
-    private Fire sut = new Fire();
+    Ship ship = new BasicShip();
+    Deck deck = new BasicDeck(ship, new DeckNumber(1));
+    Room room = new BasicRoom(1, 1, 0, 0);
+    LifeSupport lifeSupport = ship.getLifeSupport();
+    Tile tile = room.getTiles().get(0);
     
     @Before
     public void setUp() throws Exception {
-        sut.ignite(0.5f);
+        tile.ignite(0.5f);
     }
 
     @Test
@@ -37,11 +46,12 @@ public class FireTest {
         float elapsedTime = 0;
         
         while (elapsedTime < 10) {
-            sut.updateFireAndReturnConsumedOxygen(0.1f, 1);
+            room.replenishOxygen(Room.MAX_OXYGEN);
+            tile.update(0.1f);
             elapsedTime += 0.1;
         }
         
-        assertEquals(Tile.MAX_FIRE, sut.getFireLevel(), 0.05f);
+        assertEquals(Tile.MAX_FIRE, tile.getFireLevel(), 0.05f);
     }
 
     @Test
@@ -49,10 +59,11 @@ public class FireTest {
         float elapsedTime = 0;
         
         while (elapsedTime < 10) {
-            sut.updateFireAndReturnConsumedOxygen(0.1f, sut.getFireLevel() - 0.1f);
+            room.consumeOxygen(0.1f);
+            tile.update(0.1f);
             elapsedTime += 0.1f;
         }
         
-        assertEquals(Tile.NO_FIRE, sut.getFireLevel(), 0.05f);
+        assertEquals(Tile.NO_FIRE, tile.getFireLevel(), 0.05f);
     }
 }
